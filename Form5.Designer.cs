3c6a34c7196d970dc4dea498d2a509cffee384a6
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2024-01-07 18:02:22 +0300

    Finished adding teachers part

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -30,6 +30,33 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Roboto", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(698, 12);
+            this.label1.Location = new System.Drawing.Point(713, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(162, 28);
             this.label1.TabIndex = 0;
@@ -53,11 +80,266 @@
             this.dataGridView1.Size = new System.Drawing.Size(680, 330);
             this.dataGridView1.TabIndex = 1;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(713, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Teacher ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(713, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(947, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 27);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(947, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Teacher Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(713, 149);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(199, 27);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(713, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Teacher Email";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(713, 227);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(199, 27);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(713, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Department";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(947, 149);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(199, 27);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(947, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Teacher Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(999, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 50);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 528);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 50);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(257, 471);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(199, 27);
+            this.textBox6.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(257, 448);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 20);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Teacher Password";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 551);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(199, 27);
+            this.textBox7.TabIndex = 19;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 471);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(199, 27);
+            this.textBox8.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 448);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Teacher Email";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(257, 405);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(199, 27);
+            this.textBox9.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(257, 382);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Teacher Name";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 382);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Teacher ID";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Roboto", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(12, 354);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(195, 28);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Update A Teacher";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 528);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(89, 20);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Department";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 404);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(79, 28);
+            this.comboBox1.TabIndex = 24;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(798, 307);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(68, 28);
+            this.comboBox2.TabIndex = 25;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Roboto", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label13.Location = new System.Drawing.Point(713, 276);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(186, 28);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Delete A Teacher";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(713, 310);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(79, 20);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "Teacher ID";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(872, 307);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 29);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1223, 619);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "Form5";
@@ -72,5 +354,32 @@
 
         private Label label1;
         private DataGridView dataGridView1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox6;
+        private Label label7;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Label label8;
+        private TextBox textBox9;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label13;
+        private Label label14;
+        private Button button3;
     }
 }
--- Form5.cs
+++ Form5.cs
@@ -35,6 +35,166 @@ namespace DatabaseOdevi3
                     // Bind the DataTable to the DataGridView
                     dataGridView1.DataSource = dataTable;
                 }
+
+                // Load Teacher IDs into the ComboBox
+                string teacherIdsQuery = "SELECT TeacherID FROM teacher ORDER BY TeacherID";
+                using (MySqlCommand command = new MySqlCommand(teacherIdsQuery, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        comboBox1.Items.Clear();
+                        comboBox2.Items.Clear();
+
+                        while (reader.Read())
+                        {
+                            int teacherId = Convert.ToInt32(reader["TeacherID"]);
+                            comboBox1.Items.Add(teacherId);
+                            comboBox2.Items.Add(teacherId);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int intValue1;
+            if (!int.TryParse(textBox1.Text, out intValue1))
+            {
+                MessageBox.Show("Please enter a valid integer for Teacher ID.");
+                return;
+            }
+
+            string stringValue1 = textBox2.Text;
+            string stringValue2 = textBox3.Text;
+
+            int intValue2;
+            if (!int.TryParse(textBox5.Text, out intValue2))
+            {
+                MessageBox.Show("Please enter a valid integer for password.");
+                return;
+            }
+
+            string stringValue3 = textBox4.Text;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string insertQuery = "INSERT INTO teacher (TeacherID, TeacherName, Email, Department, password) VALUES (@Value1, @Value2, @Value3, @Value4, @Value5)";
+                using (MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("@Value1", intValue1);
+                    insertCommand.Parameters.AddWithValue("@Value2", stringValue1);
+                    insertCommand.Parameters.AddWithValue("@Value3", stringValue2);
+                    insertCommand.Parameters.AddWithValue("@Value4", stringValue3);
+                    insertCommand.Parameters.AddWithValue("@Value5", intValue2 );
+
+                    try
+                    {
+                        insertCommand.ExecuteNonQuery();
+                        MessageBox.Show("Record inserted successfully!");
+
+                        // Reload data and update the DataGridView
+                        LoadData();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error inserting record: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int teacherId;
+            if (!int.TryParse(comboBox1.Text, out teacherId))
+            {
+                MessageBox.Show("Please select a valid Teacher ID.");
+                return;
+            }
+
+            // Get values from TextBoxes
+            string teacherName = textBox9.Text;
+            string email = textBox8.Text;
+            string department = textBox7.Text;
+            int password;
+            if (!int.TryParse(textBox6.Text, out password))
+            {
+                MessageBox.Show("Please enter a valid integer for password.");
+                return;
+            }
+
+            // Update the teacher in the Teacher table
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string updateQuery = "UPDATE teacher SET TeacherName = @TeacherName, Email = @Email, Department = @Department, Password = @Password WHERE TeacherID = @TeacherID";
+                using (MySqlCommand updateCommand = new MySqlCommand(updateQuery, connection))
+                {
+                    updateCommand.Parameters.AddWithValue("@TeacherID", teacherId);
+                    updateCommand.Parameters.AddWithValue("@TeacherName", teacherName);
+                    updateCommand.Parameters.AddWithValue("@Email", email);
+                    updateCommand.Parameters.AddWithValue("@Department", department);
+                    updateCommand.Parameters.AddWithValue("@Password", password);
+
+                    try
+                    {
+                        updateCommand.ExecuteNonQuery();
+                        MessageBox.Show("Teacher updated successfully!");
+
+                        // Reload data and update the DataGridView
+                        LoadData();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error updating teacher: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a Teacher ID to delete.");
+                return;
+            }
+
+            int teacherId = Convert.ToInt32(comboBox2.SelectedItem);
+
+            // Confirm deletion with the user
+            DialogResult result = MessageBox.Show("Are you sure you want to delete this teacher?", "Confirmation", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                // Perform the deletion
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Execute the DELETE query
+                    string deleteQuery = "DELETE FROM teacher WHERE TeacherID = @TeacherID";
+                    using (MySqlCommand deleteCommand = new MySqlCommand(deleteQuery, connection))
+                    {
+                        deleteCommand.Parameters.AddWithValue("@TeacherID", teacherId);
+
+                        try
+                        {
+                            deleteCommand.ExecuteNonQuery();
+                            MessageBox.Show("Teacher deleted successfully!");
+
+                            // Reload data and update the DataGridView
+                            LoadData();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error deleting teacher: " + ex.Message);
+                        }
+                    }
+                }
             }
         }
     }
